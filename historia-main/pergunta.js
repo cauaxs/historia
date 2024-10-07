7e1d3db5b497e9f8617bf74920f2408a605f4512
caua <148370571+cauaxs@users.noreply.github.com>
2024-10-07 11:00:16 -0300

    Add files via upload

---
+++ historia-main/pergunta.js
@@ -0,0 +1,43 @@
+criaCartao(
+    'História',
+    'A Revolução Francesa começou em 1789',
+    'verdadeiro'
+)
+criaCartao(
+    'História',
+    'O Império Romano nunca caiu',
+    "falso"
+)
+criaCartao(
+    'História',
+    'Mahatma Gradhi foi um líder importante na luta pela independência da Índia',
+    'verdadeiro'
+)
+criaCartao(
+    'História',
+    'A Segunda Guerra Mundial começou em 1939 e terminou em 1945',
+    'verdadeiro'
+)
+criaCartao(
+    'Hitória',
+    'George Washington foi o primeiro presidente do eua',
+    'verdadeiro'
+)
+
+criaCartao(
+    'Hitória',
+    'A Guerra Civil Àmericana durou 5 anos',
+    'falso'
+)
+
+criaCartao(
+    'Hitória',
+    'Em 1776 foi assinada a Declaração Dos Estados Unidos',
+    "verdadeiro"
+)
+
+criaCartao(
+    'Hitória',
+    'Critóvão Colombo foi o famoso explorador que chegou  á America em 1492',
+    'verdadeiro'
+)
